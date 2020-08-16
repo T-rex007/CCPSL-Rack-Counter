b0d94804e0ed900dd1449b73e38df8f35db07970
T-rex007 <shaqc777@yahoo.com>
2020-08-16 19:20:05 -0400

    Model Definition

--- helper.py
+++ helper.py
@@ -5,7 +5,8 @@ from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from tensorflow.keras.models import model_from_json
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_pdf import PdfPages
-
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, Activation, MaxPooling2D, Dropout
 
 
 def cropAndPredict(frame1, r1, loaded_model):
@@ -41,3 +42,39 @@ def produceDataPdf(df):
     pp.savefig(fig, bbox_inches='tight', dpi = 5)
     pp.close()
 
+def RackNet():
+        ### CREATE SEQUENTIAL MODEL
+
+    input_shape = (64, 64, 3)
+    print (input_shape)
+    print('Creating Model')
+
+    model = Sequential()
+
+    #filters,kernel_size,strides=(1, 1),padding='valid',data_format=None,dilation_rate=(1, 1),activation=None,use_bias=True,
+    #kernel_initializer='glorot_uniform',bias_initializer='zeros',kernel_regularizer=None,bias_regularizer=None,
+    #activity_regularizer=None,kernel_constraint=None,bias_constraint=None,
+
+    #pool_size=(2, 2), strides=None, padding='valid',data_format=None
+
+    model.add(Conv2D(32, (3,3),padding='same',input_shape=input_shape,name='conv2d_1'))
+    model.add(Activation('relu'))
+    #model.add(MaxPooling2D(pool_size=(2, 2),name='maxpool2d_1'))
+
+    model.add(Conv2D(64, 3, 3, name='conv2d_6'))
+    model.add(Activation('relu'))
+    #model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.5))
+
+    model.add(Flatten())
+    model.add(Dense(64))
+    model.add(Activation('relu'))
+    model.add(Dense(32))
+    #model.add(Dropout(0.5))
+    model.add(Dense(1)) #config.num_classes
+    model.add(Activation('sigmoid'))
+
+    model.compile(loss='binary_crossentropy',
+                optimizer='Adam',
+                metrics=['accuracy'])
+    return model
