c9d1657c47e6ae954b09a04b4644dd055c20204d
T-rex007 <shaqc777@yahoo.com>
2020-08-25 00:37:35 -0400

    ------

--- launcher.sh
+++ launcher.sh
@@ -4,6 +4,6 @@
 # then to this directory, then execute python script, then back home
 
 cd /
-cd home/pi/workspace/
+cd /home/pi/workspace/CCPSL-Rack-Counter/
 sudo python3 run_rack_counter.py
 cd /
